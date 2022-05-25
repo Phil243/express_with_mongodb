78433ca97825054300b11e2c803e56de142dcb0d
Philipp Strauß <phil13.strauss@gmail.com>
2022-05-25 14:34:09 +0200

    create newStudent controller

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -1,9 +1,18 @@
+import student from "../models/student";
 
 export const getAllStudents = (req, res) => {};
 
 export const getSingleStudent = (req, res) => {};
 
-export const createNewStudent = (req, res) => {};
+export const createNewStudent = async (req, res) => {
+    try {
+        const { name, first_name, email } = req.body;
+        const newStudent = await student.create({ name, first_name, email });
+        res.status(201).json(newStudent);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const deleteStudent = (req, res) => {};
 
